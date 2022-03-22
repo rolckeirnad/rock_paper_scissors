c447635e1d1e60b8717c40fa5450b9a3ce8587ab
Daniel Gallardo <dgallardo.c@outlook.com>
2022-03-21 20:15:47 -0600

    Update game and playRound functions, add counter to display final message

--- index.js
+++ index.js
@@ -5,47 +5,32 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    //playerSelection = playerSelection.slice(0,1) + playerSelection.slice(1).toLowerCase();
-    let result, winnerSelection, looserSelection;
-
     //If player selection is equal to computer selection, it is a tie
-    if (playerSelection == computerSelection) return "It's a tie";
-
+    let winner;
+    if (playerSelection == computerSelection) winner = "Tie";
     //If player selection is Rock and computer selection is Scissors, player won.
     if (playerSelection == "Rock" && computerSelection == "Scissors") {
-        result = "Won";
-        winnerSelection = playerSelection;
-        looserSelection = computerSelection;
+        winner = "Player"
         //Else player loose and computer won.
     } else {
-        result = "Loose";
-        winnerSelection = computerSelection;
-        looserSelection = playerSelection;
+        winner = "Computer"
     }
     //If player selection is Scissors and computer selection is Paper, player won.
     if (playerSelection == "Scissors" && computerSelection == "Paper") {
-        result = "Won";
-        winnerSelection = playerSelection;
-        looserSelection = computerSelection;
+        winner = "Player"
         //Else player loose and computer won.
     } else {
-        result = "Loose";
-        winnerSelection = computerSelection;
-        looserSelection = playerSelection;
+        winner = "Computer"
     }
     //If player selection is Paper and computer selection is Rock, player won.
     if (playerSelection == "Paper" && computerSelection == "Rock") {
-        result = "Won";
-        winnerSelection = playerSelection;
-        looserSelection = computerSelection;
+        winner = "Player"
         //Else player loose and computer won.
     } else {
-        result = "Loose";
-        winnerSelection = computerSelection;
-        looserSelection = playerSelection;
+        winner = "Computer"
     }
 
-    return `You ${result}! ${winnerSelection} beats ${looserSelection}`
+    return winner;
 }
 
 function capitalize(str) {
@@ -55,20 +40,32 @@ function capitalize(str) {
 function getInput() {
     const allowedInputs = ["Rock", "Paper", "Scissors"];
     let playerInput = capitalize(prompt("Write what you choose: "));
-    console.log("normal", playerInput)
     let isValid = allowedInputs.includes(playerInput);
     while (!isValid) {
         playerInput = capitalize(prompt("Write a valid input: "));
-        console.log("error", playerInput)
         isValid = allowedInputs.includes(playerInput);
     }
     return playerInput;
 }
 
 function game() {
+    let playerRounds = 0;
+    let computerRounds = 0;
     for (let i = 0; i < 5; i++) {
         const playerSelection = getInput();
         const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection), "--", playerSelection, computerSelection);
+        const result = playRound(playerSelection, computerSelection);
+        if (result == "Player") {
+            console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+            playerRounds++;
+        } else {
+            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+            computerRounds++;
+        }
     }
+    const winner = playerRounds > computerRounds ?
+        `You win. You won ${playerRounds} rounds` :
+        `You lose. You lost ${computerRounds} rounds`;
+    console.log(winner);
+    return winner;
 }
